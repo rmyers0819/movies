5f1f98bb1766a5609e7cc4ffdece7262ece916b0
Robby M <robbykmyers@gmail.com>
2021-03-03 22:40:45 -0700

    Initial Project

---
+++ Movies/Movies/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Movies
+//
+//  Created by Robert Myers on 3/3/21.
+//  Copyright © 2021 KarSync LLC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
